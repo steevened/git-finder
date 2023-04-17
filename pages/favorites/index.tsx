d6a231305219a33b3c2f25d214cd3732fce3f796
steven <alvaradosteven315@gmail.com>
2023-04-17 17:35:52 -0500

    add suggestion when there"s no favorites on the favorites page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -5,6 +5,8 @@ import CardsGridContainer from '@/components/ui/CardsGridContainer';
 import { UserOnFavorites } from '@/lib/interfaces/user.interface';
 import { localFavourites } from '@/lib/utils';
 import UserCard from '@/components/ui/UserCard';
+import addFav from '../../public/addfav.png';
+import Image from 'next/image';
 
 const FavoritesPage: NextPageWithLayout = () => {
   const [favorites, setFavorites] = useState<UserOnFavorites[]>([]);
@@ -15,6 +17,17 @@ const FavoritesPage: NextPageWithLayout = () => {
 
   return (
     <div className="my-10">
+      {favorites.length === 0 && (
+        <div className="flex flex-col items-center justify-center w-full max-w-lg mx-auto gap-5">
+          <h3 className="text-2xl font-semibold text-center text-blue-gray-700 dark:text-blue-gray-200">
+            Ready to add your favorite profiles? Simply click the start icon
+            next to their name to save them to your favorites.
+          </h3>
+          <div className=" aspect-square w-full max-w-[200px] shadow-lg shadow-black/20 rounded-lg overflow-hidden">
+            <Image src={addFav} alt="add favorites image" />
+          </div>
+        </div>
+      )}
       <CardsGridContainer>
         {favorites.map((user) => (
           <UserCard
